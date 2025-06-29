f6ec42c62365d43d09e226648298fa5117067c14
Jim Bun <fatahulqorib@gmail.com>
2025-06-30 02:03:08 +0700

    update konten layout

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 w-full bg-black text-white px-4 py-2 flex gap-4 justify-center">
+      <a href="#hero">Home</a>
+      <a href="#about">Tentang</a>
+      <a href="#skills">Keahlian</a>
+      <a href="#projects">Proyek</a>
+      <a href="#contact">Kontak</a>
+    </nav>
+  );
+}
